4bd4cb6ccd6da62c6d2dc46d3980c14ad7aebf26
HERNÁN CAPRA <hernancapra@economicas.uba.ar>
2021-04-15 19:32:01 -0300

    Agregar archivos de proyecto.

---
+++ A881457.Actividad01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A881457.Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo.");
+            Console.ReadKey();
+        }
+    }
+}
